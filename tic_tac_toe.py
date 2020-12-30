d0e75b2248fb70bf7ddfb53853b2b99fcc492c5d
root <czapski.michal@gmail.com>
2020-12-30 00:58:49 +0000

    skeleton class

---
+++ tic_tac_toe.py
@@ -0,0 +1,36 @@
+class Game:
+    
+    def __init__(self):
+        self.player1=input("What is player one's name?"\n)
+        self.player2=input("What is player one's name?"\n)
+        self.game_counter=0
+        self.scores={}
+        
+    
+    def play(self):
+        
+        """Start tic tac toe session
+        
+        Args:
+        None
+        Returns: 
+        none
+        
+        """
+        
+        
+    def ask_move(self):
+        pass
+    
+    def print_board(self):
+        pass
+    
+    def check_status(self):
+        pass
+        
+    def print_results(self):
+        pass
+        
+      
+
+    
